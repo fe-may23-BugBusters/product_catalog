417b6ce72b39da2307494c46725f3fda79ad6820
Piotr <piotrkrasucki@gmail.com>
2023-10-26 16:57:56 +0200

    husky-test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className=
+      "App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
