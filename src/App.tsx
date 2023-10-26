80231c1a8c8f560ba4d77d5af65a2d9de314416b
Sviatoslav Vasyletskyi <ditchqiwi@gmail.com>
2023-10-26 22:52:59 +0200

    add router /phone

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,36 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import PhonesPage from './PhonePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Strona główna</Link>
+              </li>
+              <li>
+                <Link to="/phones">Strona z telefonami</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Route path="/phones" Component={PhonesPage} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/PhonesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PhonesPage() {
+  return (
+    <div>
+      <h1>Strona z telefonami</h1>
+    </div>
+  );
+}
+
+export default PhonesPage;
