b8a52f42b966928b6ce246d5a7538350a60a6151
arturwisnia <wisnia3008@interia.pl>
2023-10-30 16:49:15 +0100

    add with footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Footer from './components/footer';
 import logo from './logo.svg';
 import './App.css';
