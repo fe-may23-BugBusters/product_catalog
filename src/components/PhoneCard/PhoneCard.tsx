a48eaed36ac31a6e5c0334c16201258d3ac06975
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-11-09 13:31:08 +0100

    sort-fix

--- src/components/PhoneCard/PhoneCard.tsx
+++ src/components/PhoneCard/PhoneCard.tsx
@@ -63,6 +63,7 @@ export const PhoneCard: React.FC<Props> = ({
         (item) => item.itemid !== product.itemid,
       );
 
+      setIsLikedClick(!isLikedClick);
       setFavourites([...newFavourites]);
     }
   };
--- src/components/PhonesPage/PhonesPage.tsx
+++ src/components/PhonesPage/PhonesPage.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable react/button-has-type */
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import PropagateLoader from 'react-spinners/PropagateLoader';
 import axios from 'axios';
 import './sass/PhonesPage.scss';
 import Home from '../../icons/Home.svg';
@@ -18,8 +19,8 @@ export const PhonesPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [perPage, setPerPage] = useState(16);
   const [totalPosts, setTotalPosts] = useState(0);
-  const [orderBy, setOrderBy] = useState('year');
-  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('id');
+  const [order, setOrder] = useState('ASC');
   const [search, setSearch] = useState('');
 
   useEffect(() => {
@@ -35,7 +36,7 @@ export const PhonesPage = () => {
 
       try {
         const response = await axios.get(
-          `https://product-catalog-be-6qo2.onrender.com/products?pageNumber=${currentPage}&perPage=${perPage}&orderBy=${orderBy}&order=${order}&search=${search}`,
+          `https://product-catalog-be-6qo2.onrender.com/products?perPage=${perPage}&pageNumber=${currentPage - 1}&orderBy=${orderBy}&order=${order}`,
         );
 
         setProducts(response.data.rows);
@@ -53,9 +54,16 @@ export const PhonesPage = () => {
   const numberOfPages = Math.ceil(totalPosts / perPage);
 
   const toggleOrder = () => {
-    const newOrder = order === 'asc' ? 'desc' : 'asc';
+    const newOrder = order === 'ASC' ? 'DESC' : 'ASC';
 
     setOrder(newOrder);
+  };
+
+  const override: any = {
+    alignSelf: 'center',
+    position: 'absolute',
+    left: '50vW',
+    marginTop: '40px',
   };
 
   return (
@@ -76,28 +84,80 @@ export const PhonesPage = () => {
           />
           <p className="phonePage__current__text">Phones</p>
         </div>
+        <div className="phonePage__select_cont">
+          <div className="phonePage__select phonePage__select--1">
+            <label
+              htmlFor="sort-by"
+              className="phonePage__text phonePage__select__text"
+            >
+              Sort by:
+            </label>
+            <select
+              name="sort-by"
+              id="sort-by"
+              className="phonePage__select__field"
+              value={orderBy}
+              onChange={(e) => setOrderBy(e.target.value)}
+            >
+              <option value="year">Year</option>
+              <option value="fullprice">Price</option>
+              <option value="id">ID</option>
+            </select>
+          </div>
+          <div className="phonePage__select phonePage__select--1">
+            <label
+              htmlFor="order"
+              className="phonePage__text phonePage__select__text"
+            >
+              Order:
+            </label>
+            <button
+              type="button"
+              id="order"
+              name="order"
+              className="phonePage__select__field"
+              onClick={() => toggleOrder()}
+            >
+              {order === 'ASC' ? 'Ascending' : 'Descending'}
+            </button>
+          </div>
 
-        <div className="phonePage__select phonePage__select--1">
-          <label
-            htmlFor="sort-by"
-            className="phonePage__text phonePage__select__text"
-          >
-            Sort by:
-          </label>
-          <select
-            name="sort-by"
-            id="sort-by"
-            className="phonePage__select__field"
-            value={orderBy}
-            onChange={(e) => setOrderBy(e.target.value)}
-          >
-            <option value="year">Year</option>
-            <option value="fullprice">Price</option>
-            <option value="id">ID</option>
-          </select>
-          <button onClick={toggleOrder}>Toggle ASC/DESC</button>
+          <div className="phonePage__select phonePage__select--1">
+            <label
+              htmlFor="item-per-page"
+              className="phonePage__text phonePage__select__text"
+            >
+              Items per page:
+            </label>
+            <select
+              name="items-per-page"
+              id="items-per-page"
+              className="phonePage__select__field"
+              value={String(perPage)}
+              onChange={(e) => setPerPage(+e.target.value)}
+            >
+              <option value="8">8</option>
+              <option value="12">12</option>
+              <option value="16">16</option>
+            </select>
+          </div>
+          {/* <div className="phonePage__select phonePage__select--1">
+            <label
+              htmlFor="search"
+              className="phonePage__text phonePage__select__text"
+            >
+              Search by name:
+            </label>
+            <input
+              type="text"
+              id="searach"
+              name="search"
+              value={search}
+              className="phonePage__select__field"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div> */}
         </div>
-
         <div className="product-card__container">
           {!loading
             ? products.map((product) => (
@@ -120,7 +180,7 @@ export const PhonesPage = () => {
                 />
               </div>
             ))
-            : 'Loading...'}
+            : <PropagateLoader color="#0F0F11" cssOverride={override} />}
         </div>
       </main>
       <Pagination
