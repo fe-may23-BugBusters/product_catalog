269d7f003808aac875407511cb9efe895f43a719
Piotr <piotrkrasucki@gmail.com>
2023-11-09 20:35:55 +0100

    fixcat

--- src/components/ActionsVariants/ActionsVariants.tsx
+++ src/components/ActionsVariants/ActionsVariants.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable max-len */
+/* eslint-disable */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
